683a7dff7975d19b75366b3be89630c656210bef
Jarviniazh <zhaojwdkj@gmail.com>
2024-05-29 07:58:42 -0400

    Finish declarations for all functions required

---
+++ legoSets.js
@@ -0,0 +1,25 @@
+const setData = require("../data/setData");
+const themeData = require("../data/themeData");
+
+
+let sets = [];
+
+// The purpose of this function is to fill the "sets" array (declared above), by adding copies of all the setData objects?
+function initialize() {
+
+};
+
+// This function simply returns the complete "sets" array
+function getAllSets() {
+
+};
+
+//This function will return a specific "set" object from the "sets" array, whose "set_num" value matches the value of the "setNum" parameter
+function getSetByNum(setNum){
+
+};
+
+//The purpose of this function is to return an array of objects from the "sets" array whose "theme" value matches the "theme" parameter
+function getSetsByTheme(theme){
+
+};
