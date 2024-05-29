92370f4b29504f6ffa66376ef5e05c7b948a5357
Jarviniazh <zhaojwdkj@gmail.com>
2024-05-29 09:30:20 -0400

    Revised version & pass functionality test

--- server.js
+++ server.js
@@ -3,39 +3,38 @@ const express = require("express");
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
 
-function getSetByNum(setNum){
-    return new Promise((resolve, reject) => {
-        let result = legoData.getSetByNum(setNum);
-        (result) ? resolve(result) : reject("No data found");
-    })
-};
+app.get("/", (req, res) => {
+    res.send("Welcome to the Lego Sets API");
+});
 
-function getSetsByTheme(theme){
-    return new Promise((resolve, reject) => {
-        let result = legoData.getSetsByTheme(theme);
-        (result) ? resolve(result) : reject("No data found");
-    })
+app.get("/lego/sets", async (req, res) => {
+    let sets = await legoData.getAllSets();
+    res.send(sets);
+});
 
-};
-
-async function getSever() {
-    try {
-        let init = await legoData.initialize();
-        console.log("Successfully initialized Lego Sets module");
-        app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
-        app.get("/", (req, res) => {res.send("Welcome to the Lego Sets API");});
-
-        let getNum = await getSetByNum("0011-2");
-        let getTheme = await getSetsByTheme("technic");
-    } catch (error) {
-        console.log(error);
+app.get("/lego/sets/num-demo", async (req, res) => {
+    
+    try{
+        let sets = await legoData.getSetByNum("0011-2");
+        res.send(sets);
     }
-}
+    catch(error){
+        res.send(error);
+    }
+});
 
-getSever();
+app.get("/lego/sets/theme-demo", async (req, res) => {
+    
+    try{
+        let sets = await legoData.getSetsByTheme("town");
+        res.send(sets);
+    }
+    catch(error){
+        res.send(error);
+    }
+});
 
+legoData.initialize().then(() => {
+    app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+});
 
-// legoData.initialize().then(() => {
-//     app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
-//     app.get("/", (req, res) => {res.send("Welcome to the Lego Sets API");}); //"Assignment 2:  Student Name - Student Id" where "Student Name" and "Student Id" 
-// });
